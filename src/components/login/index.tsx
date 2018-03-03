cc74eb1dbf06f1fff71bc532634bd5885b8f6f50
Boan Liang <anboxer@163.com>
2018-03-03 17:08:25 +0800

    rollback webpack to 3.X

---
+++ src/components/login/index.tsx
@@ -0,0 +1,11 @@
+import * as React from "react"
+interface ILoginProps {
+    t?: string
+}
+export class Login extends React.PureComponent<ILoginProps> {
+    public render() {
+        return (
+            <div>Login</div>
+        );
+    }
+}

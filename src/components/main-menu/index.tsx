9a057e9c70eb892f84271721180c20b6f58ba582
Boan Liang <liangboxan@gmail.com>
2018-04-24 00:00:16 +0800

    remove menu

--- src/components/main-menu/index.tsx
+++ src/components/main-menu/index.tsx
@@ -1,7 +1,5 @@
 import * as React from "react"
 import { Menu } from 'antd';
-import { Link } from "react-router-dom"
-import { FormattedMessage } from "react-intl";
 
 import { injectIntlDecorator } from "../base";
 
@@ -14,21 +12,7 @@ export class MainMenu extends React.PureComponent {
                 mode="horizontal"
                 style={{ lineHeight: '64px' }}
             >
-                <Menu.Item key="1">
-                    <Link to="/analysis" >
-                        <FormattedMessage id="global.analysis" />
-                    </Link>
-                </Menu.Item>
-                <Menu.Item key="2">
-                    <Link to="/dashboard" >
-                        <FormattedMessage id="global.dashboard" />
-                    </Link>
-                </Menu.Item>
-                <Menu.Item key="3">
-                    <Link to="/settings" >
-                        <FormattedMessage id="global.settings" />
-                    </Link>
-                </Menu.Item>
+
             </Menu>
         )
     }
